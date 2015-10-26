0cc28c0b95aefe71248401a11ab22c3681b97a82
Andreas Greger <344A83BCA624F55F@mytum.de>
2015-10-26 13:45:58 +0100

    Arranged the Activity-order so that the Splashscreen comes first, then the login and afterwards the main

---
+++ app/src/main/java/de/smartdev/application_nosensitivity/LoginActivity.java
@@ -0,0 +1,43 @@
+package de.smartdev.application_nosensitivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static int zeit = 8000; // TODO: change from time to on sucess login
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, zeit);
+
+    }
+
+}
